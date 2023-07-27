ed22c43d87a8e9c056ba03ab40c91e4202c825a3
mostafagh79 <mostafa.gh.79a@gmail.com>
2023-07-27 21:14:12 +0330

    Feature : downside-alignment

---
+++ src/components/drawer/DownSideAlignment.jsx
@@ -0,0 +1,25 @@
+import { Icon } from "../../layout/Icon"
+import textAlignJustifyCenter from "../../assets/images/text-align-justify-light.svg"
+import textAlignLeft from "../../assets/images/text-align-left-fill.svg"
+import textAlignCenter from "../../assets/images/text-center.svg"
+import textAlignRight from "../../assets/images/text-right.svg"
+import textBold from "../../assets/images/bold-text-outline-icon.svg"
+import textItalic from "../../assets/images/text-italic-thin.svg"
+import textUnderline from "../../assets/images/text-underline-thin.svg"
+
+
+function DownSideAlignment() {
+  return (
+    <div className="flex flex-row h-fit gap-3 ml-20 mt-20">
+        <Icon href={"#"} alt={"textalign-justifycenter"} iconSrc={textAlignJustifyCenter}></Icon>
+        <Icon href={"#"} alt={"textalign-right"} iconSrc={textAlignLeft}></Icon>
+        <Icon href={"#"} alt={"textalign-center"} iconSrc={textAlignCenter}></Icon>
+        <Icon href={"#"} alt={"textalign-left"} iconSrc={textAlignRight}></Icon>
+        <Icon href={"#"} alt={"text-bold"} iconSrc={textBold}></Icon>
+        <Icon href={"#"} alt={"text-italic"} iconSrc={textItalic}></Icon>
+        <Icon href={"#"} alt={"text-underline"} iconSrc={textUnderline}></Icon>
+    </div>
+  )
+}
+
+export  {DownSideAlignment}
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,3 +1,4 @@
+import { DownSideAlignment } from "../components/drawer/DownSideAlignment"
 import { TemplateNavbar } from "./TemplateNavbar"
 import  TemplateSideBar  from "./TemplateSideBar"
 
@@ -5,7 +6,10 @@ function Layout() {
   return (
     <div className="bg-gray-300 h-screen w-full">
         <TemplateNavbar/>
-        <main className="relative w-full h-[calc(100%-56px)]"><TemplateSideBar/></main>
+        <main className="relative w-full h-[calc(100%-56px)]">
+          <TemplateSideBar/>
+          <DownSideAlignment/>
+          </main>
     </div>
   )
 }
