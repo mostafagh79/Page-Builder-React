e716d567005e5326f833e2e5c16323bd7c9d2734
mostafagh79 <mostafa.gh.79a@gmail.com>
2023-07-27 16:28:59 +0330

    Feature : dropbox

---
+++ src/components/drawer/DropBoxIcons.jsx
@@ -0,0 +1,14 @@
+
+
+function DropBoxIcons(props) {
+  return (
+     <a href={props.href} className="btn-middle">
+      <img
+        src={props.iconSrc}
+        alt={props.alt}
+      />
+    </a>
+  )
+}
+
+export  {DropBoxIcons}
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,3 +1,4 @@
+import { DropBox } from "../components/drawer/DropBox"
 import { TemplateNavbar } from "./TemplateNavbar"
 import  TemplateSideBar  from "./TemplateSideBar"
 
@@ -5,7 +6,11 @@ function Layout() {
   return (
     <div className="bg-gray-300 h-screen w-full">
         <TemplateNavbar/>
-        <main className="relative w-full h-[calc(100%-56px)]"><TemplateSideBar/></main>
+        <main className="relative w-full h-[calc(100%-56px)]">
+          <TemplateSideBar/>
+          <DropBox/>
+        </main>
+        
     </div>
   )
 }
