e6c37b15f68cc33b5fda54b0aa24669d498241ff
mostafagh79 <mostafa.gh.79a@gmail.com>
2023-07-28 20:09:17 +0330

    Feature : switch toggle

--- src/components/drawer/Switch.jsx
+++ src/components/drawer/Switch.jsx
@@ -1,10 +1,38 @@
+import { useState } from "react";
 
 function Switch() {
+
+     const [isChecked, setIsChecked] = useState(false);
+
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+  };
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <label className="flex cursor-pointer select-none items-center">
+        <div className="relative">
+          <input
+            type="checkbox"
+            checked={isChecked}
+            onChange={handleCheckboxChange}
+            className="sr-only"
+          />
+          <div
+            className={`box block h-8 w-14 rounded-full ${
+              isChecked ? "bg-blue-700" : "bg-slate-500"
+            }`}
+          ></div>
+          <div
+            className={`switch ${
+              isChecked ? "translate-x-full" : ""
+            }`}
+          ></div>
+        </div>
+      </label>
+    </>
+ 
   );
 }
 
-export  {Switch}
+export {Switch}
