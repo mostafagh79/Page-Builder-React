fac66b454d68e2df203c8db9db9351d2f4120013
mostafagh79 <mostafa.gh.79a@gmail.com>
2023-07-29 13:40:10 +0330

    colorbox feature

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,3 +1,4 @@
+import ColorBox from "../components/drawer/ColorBox";
 import { TemplateNavbar } from "./TemplateNavbar"
 import  TemplateSideBar  from "./TemplateSideBar"
 
@@ -5,12 +6,11 @@ import  TemplateSideBar  from "./TemplateSideBar"
 function Layout() {
   return (
     <div className="bg-gray-300 h-screen w-full">
-        <TemplateNavbar/>
-        <main className="relative w-full h-[calc(100%-56px)]">
-          <TemplateSideBar/>
-        </main>
-
-
+      <TemplateNavbar />
+      <main className="relative w-full h-[calc(100%-56px)]">
+        <TemplateSideBar />
+        <ColorBox />  
+      </main>
 
     </div>
   );
